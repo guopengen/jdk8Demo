6a886ab21bb3ab15c6d96d4eafeeabe123be6c6b
wangpengfei101022 <wangpengfei101022@jd.com>
2020-01-21 21:56:00 +0800

    master1

---
+++ src/main/java/com/imooc/Test2.java
@@ -0,0 +1,15 @@
+package com.imooc;
+
+/**
+ * @Description
+ * @Author wangpengfei101022
+ * @Date 2020/1/14 9:58
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println(
+
+        );
+        System.out.println("I am test2");
+    }
+}
